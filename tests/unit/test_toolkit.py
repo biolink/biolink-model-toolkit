f33b44a0a020bad1c2df72242322b3f219e43b7d
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-06-17 12:59:45 -0700

    adding more tests

--- tests/unit/test_toolkit.py
+++ tests/unit/test_toolkit.py
@@ -118,6 +118,9 @@ def test_get_element(toolkit):
     o = toolkit.get_element('molecular_function')
     assert o and o.name == MOLECULAR_ACTIVITY
 
+    o = toolkit.get_element('cellular_component')
+    assert o and o.name == 'cellular component'
+
     o = toolkit.get_element('RNA Product')
     assert o and o.name == 'RNA product'
 
