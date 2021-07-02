eff1e1afa981e89a4c83f11cbf0f1dff5b8d7eed
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-07-02 17:02:36 -0400

    Add name formatting tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,11 @@
+from bmt.toolkit import Toolkit
 import pytest
 
-from bmt.utils import parse_name
+from bmt.utils import parse_name, format_element, sentencecase_to_camelcase
+
+@pytest.fixture(scope="module")
+def toolkit():
+    return Toolkit()
 
 
 @pytest.mark.parametrize('query', [
@@ -23,3 +28,22 @@ def test_parse_name(query):
     n = parse_name(query[0])
     assert n == query[1]
 
+
+@pytest.mark.parametrize('query', [
+    ('phenotypic feature', 'PhenotypicFeature'),
+    ('noncoding RNA product', 'NoncodingRNAProduct'),
+])
+def test_sentencecase_to_camelcase(query):
+    n = sentencecase_to_camelcase(query[0])
+    assert n == query[1]
+
+
+@pytest.mark.parametrize('query', [
+    ('related to', 'biolink:related_to'),
+    ('caused_by', 'biolink:caused_by'),
+    ('PhenotypicFeature', 'biolink:PhenotypicFeature'),
+    ('noncoding RNA product', 'biolink:NoncodingRNAProduct'),
+])
+def test_format_element(query, toolkit):
+    n = format_element(toolkit.get_element(query[0]))
+    assert n == query[1]
