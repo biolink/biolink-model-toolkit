3c8f7f18ca4e11de2053562bcd53b5e199aa5317
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-01-04 11:33:39 -0800

    add test

---
+++ tests/unit/test_toolkit_generator.py
@@ -0,0 +1,12 @@
+import pytest
+from bmt import ToolkitGenerator
+
+REMOTE_PATH = (
+    "https://raw.githubusercontent.com/biolink/biolink-model/2.2.12/biolink-model.yaml"
+)
+
+
+def test_get_generator():
+    gen = ToolkitGenerator(REMOTE_PATH)
+    tk = gen.serialize()
+    print(tk)
