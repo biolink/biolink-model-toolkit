51012ec88e20088512509297bf94981a1ef04d96
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-11-11 11:05:41 -0800

    tweaking docs

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -53,3 +53,5 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
+# Options for autosectionlabel
+autosectionlabel_prefix_document = True
