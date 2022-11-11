c105362fea452d1ad13fe158a6d76f6e5651ae44
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-11-11 09:21:58 -0800

    trying to get docs to deploy with poetry

--- docs/conf.py
+++ docs/conf.py
@@ -1,46 +1,81 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Path setup --------------------------------------------------------------
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Biolink Model Toolkit'
-copyright = '2022, Sierra Moxon; Deepak Unni'
-author = 'Sierra Moxon; Deepak Unni'
-release = '0.8.12'
+
+# -- Project information -----------------------------------------------------
+
+project = 'linkml'
+copyright = '2021, LinkML Authors'
+author = 'LinkML Authors'
+
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 
 extensions = [
-   'sphinx.ext.autodoc',
-   'sphinx.ext.githubpages',
-   'sphinx_rtd_theme',
-   'recommonmark'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_click',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'myst_parser'
 ]
 
+myst_heading_anchors = 3
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.md']
+
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+html_logo = 'https://linkml.io/uploads/linkml-logo_color.png'
 
-html_context = {
-    # "github_url": "https://github.com", # or your GitHub Enterprise interprise
-    "github_user": "bmt",
-    "github_repo": "biolink-model-toolkit",
-    "github_version": "master/docs/",
-    "display_github": True,  # Add 'Edit on Github' link instead of 'View page source'
-    "conf_py_path": "/master/docs/", # Path in the checkout to the docs root
-}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+
+
+# Options for the linkcheck builder
+linkcheck_ignore = [
+    'https://w3id.org/linkml/*',
+]
+
+# Options for autosectionlabel
+autosectionlabel_prefix_document = True
