9f3b864fb0963167d364e485b1a5731d2be931a3
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-11-11 09:12:12 -0800

    setting a path to reflect other repos

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Biolink Model Toolkit'
 copyright = '2022, Sierra Moxon; Deepak Unni'
