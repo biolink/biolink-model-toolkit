de5ab4736039806756bfdef3676503f99d40ff9d
richardbruskiewich <richard.bruskiewich@delphinai.com>
2023-02-08 16:14:46 -0800

    Add CURIE qualifier_type_id use case to validate_qualifier unit test  (also removed some spurious line comments)

--- tests/unit/test_toolkit.py
+++ tests/unit/test_toolkit.py
@@ -261,12 +261,13 @@ def test_is_enum(toolkit):
 
 def test_is_reachable_from_enum(toolkit):
     assert toolkit.is_reachable_from_enum(ANATOMICAL_CONTEXT_QUALIFIER_ENUM_NAME, "UBERON:0001981")  # Blood Vessel
-    assert not toolkit.is_reachable_from_enum(ANATOMICAL_CONTEXT_QUALIFIER_ENUM_NAME, "pax:0001981")  # Blood Vessel
+    assert not toolkit.is_reachable_from_enum(ANATOMICAL_CONTEXT_QUALIFIER_ENUM_NAME, "pax:0001981")
 
 
 def test_validate_qualifier(toolkit):
-    assert toolkit.validate_qualifier(SUBJECT_DIRECTION_QUALIFIER_NAME, "upregulated")  # Blood Vessel
-    assert not toolkit.validate_qualifier(SUBJECT_DIRECTION_QUALIFIER_NAME, "pax:0001981")  # Blood Vessel
+    assert toolkit.validate_qualifier(SUBJECT_DIRECTION_QUALIFIER_NAME, "upregulated")
+    assert not toolkit.validate_qualifier(SUBJECT_DIRECTION_QUALIFIER_NAME, "pax:0001981")
+    assert toolkit.validate_qualifier(SUBJECT_DIRECTION_QUALIFIER_CURIE, "upregulated")
 
 
 def test_is_permissible_value_of_enum(toolkit):
