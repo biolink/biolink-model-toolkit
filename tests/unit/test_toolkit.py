939f2fc331b6e1471af2626b26816d38c3a416b6
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-03-27 17:22:55 -0700

    update test to reflect biolink model update

--- tests/unit/test_toolkit.py
+++ tests/unit/test_toolkit.py
@@ -66,7 +66,7 @@ BIOLINK_ENTITY = 'biolink:Entity'
 
 def test_get_model_version(toolkit):
     version = toolkit.get_model_version()
-    assert version == "3.2.0"
+    assert version == "3.2.2"
 
 
 def test_get_id_prefixes(toolkit):
