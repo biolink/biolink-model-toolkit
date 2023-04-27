e343591af014e2ac2f71749ad9738d6109d42aed
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2023-04-27 14:49:21 -0700

    update to 3.3.1

--- tests/unit/test_toolkit.py
+++ tests/unit/test_toolkit.py
@@ -66,7 +66,7 @@ BIOLINK_ENTITY = 'biolink:Entity'
 
 def test_get_model_version(toolkit):
     version = toolkit.get_model_version()
-    assert version == "3.3.0"
+    assert version == "3.3.1"
 
 
 def test_get_denormalized_association_slots(toolkit):
